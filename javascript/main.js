efcfebbbe9726bb85a5c0f580e742c3cd0cc2f21
baraa osama <baraaosama038@gmail.com>
2022-08-15 11:23:32 +0200

    make landing page

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+const mobileMenuIcon = document.querySelector(".menu-icon");
+const mobileMainList = document.querySelector(".mobile-menu");
+const content = document.querySelector(".content");
+const x = window.matchMedia("(max-width: 737px)")
+mobileMenuIcon.addEventListener("click", () => {
+    if (mobileMainList.style.display == "block") {
+        mobileMainList.style.display = "none";
+        mobileMenuIcon.src = "images/icon-hamburger.svg";
+        content.style.display = "flex"
+    } else {
+        mobileMainList.style.display = "block";
+        mobileMenuIcon.src = "../images/icon-close.svg"
+        content.style.display = "none"
+    }
+
+})
